039e52845e69866049f9fcffc018801a1cf4059f
Bryan <byaespinosag@poligran.edu.co>
2026-08-19 09:37:09 -0500

    Soporte SSL para SMTP de Hostinger

--- config/settings.py
+++ config/settings.py
@@ -148,10 +148,11 @@ LOGOUT_REDIRECT_URL = "home"
 if os.getenv("EMAIL_HOST"):
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = os.getenv("EMAIL_HOST")
-    EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", "465"))
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True"
+    EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "True"
     DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
 else:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
