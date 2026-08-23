72fbf9ff89d829d0a49fcd511a9af74bb437ac89
Bryan <byaespinosag@poligran.edu.co>
2026-08-22 22:09:18 -0500

    Usar Brevo via API HTTP en vez de SMTP (Railway bloquea SMTP saliente)

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "pets",
     "orders",
     "backoffice",
+    "anymail",
 ]
 
 MIDDLEWARE = [
@@ -145,15 +146,12 @@ LOGIN_REDIRECT_URL = "pets:my_pets"
 LOGOUT_REDIRECT_URL = "home"
 
 # Email -------------------
-if os.getenv("EMAIL_HOST"):
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = os.getenv("EMAIL_HOST")
-    EMAIL_PORT = int(os.getenv("EMAIL_PORT", "465"))
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True"
-    EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "True"
-    DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
+if os.getenv("BREVO_API_KEY"):
+    EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
+    ANYMAIL = {
+        "BREVO_API_KEY": os.getenv("BREVO_API_KEY"),
+    }
+    DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "info@idpetscan.com")
 else:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
