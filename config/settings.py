adc046a8734666721870d5352cc3d85fc6388841
Bryan <byaespinosag@poligran.edu.co>
2026-08-25 19:10:29 -0500

    Fix: usar storage estandar de Django para archivos estaticos (evita error de WhiteNoise)

--- config/settings.py
+++ config/settings.py
@@ -132,10 +132,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
