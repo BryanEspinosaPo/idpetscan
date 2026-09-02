cd916d79354c4f3f0f9800a0fa201adaffb7452e
Bryan <byaespinosag@poligran.edu.co>
2026-09-02 16:42:38 -0500

    Agregar sitemap.xml (solo paginas publicas) y proteger perfiles de mascotas de indexacion

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path("robots.txt", TemplateView.as_view(
         template_name="robots.txt", content_type="text/plain",
     ), name="robots_txt"),
+    path("sitemap.xml", TemplateView.as_view(
+        template_name="sitemap.xml", content_type="application/xml",
+    ), name="sitemap_xml"),
     path("politica-de-datos/", TemplateView.as_view(
         template_name="legal/politica_datos.html",
         extra_context={"admin_email": settings.ADMIN_NOTIFICATION_EMAIL},
