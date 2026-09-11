60eadf46bd51d0c52741031800c10ea9703593b3
Bryan <byaespinosag@poligran.edu.co>
2026-09-10 20:58:44 -0500

    Seguridad: agregar HSTS y hardening adicional

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,11 @@ if not DEBUG:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = False
+    SESSION_COOKIE_HTTPONLY = True
+    X_FRAME_OPTIONS = "DENY"
 
 # --------------------------------------------------------------------
 # Apps instaladas
